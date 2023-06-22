45d3a85f5cd536a16c84ffbed88c532cfe85e294
luisd2023 <137340894+luisd2023@users.noreply.github.com>
2023-06-22 10:59:53 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: ["./src/**/*.hbs"],
-  content: ["./src/**/*.{html,js,hbs}", "./views/*.hbs"],
+  content: ["./build/**/*.{html,js,hbs}","./src/**/*.{html,js,hbs}" "./views/*.hbs,html,js"],
   theme: {
     extend: {},
   },
