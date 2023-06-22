c2b43ec3374887eb141eca30e33bb72b44f7d071
luisd2023 <137340894+luisd2023@users.noreply.github.com>
2023-06-21 22:18:53 -0500

    Delete src directory
    dek

--- src/database.js
+++
@@ -1,7 +0,0 @@
-import { MONGODB_URI } from "./config";
-import mongoose from "mongoose";
-
-mongoose
-  .connect(MONGODB_URI)
-  .then((db) => console.log("Db is connected"))
-  .catch((error) => console.log(error));
--- src/middlewares/index.js
+++
@@ -1,4 +0,0 @@
-import * as authJwt from "./authjwt";
-import * as verifySign from "./verifySign";
-
-export { authJwt, verifySign };
